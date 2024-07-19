1c159ecc51b2fbc86e09f9597ad18b1492a2180f
Sethis <80702140+Sethis@users.noreply.github.com>
2024-07-20 03:54:03 +0400

    Update router.py

--- tests/router.py
+++ tests/router.py
@@ -57,6 +57,10 @@ def test_dublicate_get():
 def test_sort_get():
     router = Router()
 
+    @router.get("some/{text}/{number}")
+    def func(text: str, number: int):
+        return text, number, 3
+
     @router.get("some/{number}/{text}")
     def func(number: int, text: str):
         return number, text, 1
@@ -65,9 +69,11 @@ def test_sort_get():
     def func(text: str):
         return text, 2
 
-    @router.get("some/{text}/{number}")
-    def func(text: str, number: int):
-        return text, number, 3
+    @router.get("some/0/{number}")
+    def func(number: int):
+        return number, 4
+
+    router.sort()
 
     handler_obj = router.route("GET", "/some/1234/value")
     handler = handler_obj[0]
@@ -91,3 +97,10 @@ def test_sort_get():
     assert result[0] == "other"
     assert result[1] == 123
     assert result[2] == 3
+
+    handler_obj = router.route("GET", "/some/0/123")
+    handler = handler_obj[0]
+    result = handler(*handler_obj[1])
+
+    assert result[0] == 123
+    assert result[1] == 4
